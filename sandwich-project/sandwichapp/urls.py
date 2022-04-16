02fa11dbdc3dcbe909ab523cc9d125f903050188
Mianta <RogueStorm7>
2022-04-16 16:51:18 -0700

    files added

---
+++ sandwich-project/sandwichapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from sandwichapp.views import SandwichappView, IngredientsView, SandwichGeneratorView 
+urlpatterns = [
+    # sandwich/
+    path("", SandwichappView.as_view(), name="sandwich"),
+    path(
+        "ingredients/<str:ingredient_type>",
+        IngredientsView.as_view(),
+        name="ingredients_list",
+    ),
+    path("random", SandwichGeneratorView.as_view(), name="sandwich_generator"),
+]
